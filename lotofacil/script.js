79186c1d21ce485e46f7847ae23ee4d621d0104f
Saulo Aguiar <sauloaguiar93@hotmail.com>
2026-08-27 22:17:17 -0300

    Update script.js

--- lotofacil/script.js
+++ lotofacil/script.js
@@ -11,7 +11,7 @@ const participantes = [
   "Sara Aguiar",
   "Antonio Oliveira",
   "Humberto Almeida"
-  ;
+  ];
 
   function displayParticipants() {
     const participantsList = document.getElementById('participantsList');
