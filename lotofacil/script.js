ffed0eb2410e05c9c05bce9978c84dc5df1d0a49
Saulo Aguiar <sauloaguiar93@hotmail.com>
2026-09-07 10:08:04 -0300

    Atualizar o script.js

--- lotofacil/script.js
+++ lotofacil/script.js
@@ -37,6 +37,7 @@ const participantes = [
   { nome: "Maia Batista" }, // Pagamento_Completo_R$95_20
   { nome: "Romero Santana" }, // Pagamento_Completo_R$95_20
   { nome: "José Pinage" }, // Pagamento_Completo_R$95_20
+  { nome: "Ivanildo Tavares" }, // Pagamento_Completo_R$95_20
   ];
 
   function displayParticipants() {
